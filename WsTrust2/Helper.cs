d3c40f87b37a9819e18f621c9ceee6418f41e5dc
jennyf19 <ferriesj@gmail.com>
2017-02-03 15:49:46 -0800

    initial commit

---
+++ WsTrust2/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WsTrust2
+{
+    internal class Helper
+    {
+        /// <summary>
+        ///     Returns a random string of upto 32 characters.
+        /// </summary>
+        /// <returns>String of upto 32 characters.</returns>
+        public static string GetRandomString(int length = 32)
+        {
+            //because GUID can't be longer than 32
+            return Guid.NewGuid().ToString("N").Substring(0, length > 32 ? 32 : length);
+        }
+    }
+}
